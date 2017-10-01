45dc8ae7296e9dcd950dc4983651f72dbbe11ced
Clément Poissonnier <clement.poissonnier@ineat-conseil.fr>
2017-10-02 00:03:20 +0200

    fix(anki-report) : strip newlines

--- app/infra/adapter/AnkiFormatter.java
+++ app/infra/adapter/AnkiFormatter.java
@@ -10,11 +10,15 @@ public class AnkiFormatter implements Formatter {
 
     @Override
     public String format(WordSearchResult searchResult) {
-        return searchResult.request + "\t" + formatMeaningsAsHtml(searchResult) + "\t";
+        return stripNewLines(searchResult.request + "\t" + formatMeaningsAsHtml(searchResult) + "\t");
     }
 
     private String formatMeaningsAsHtml(WordSearchResult searchResult) {
         String meaningList = searchResult.meanings != null ? "<ul>" + searchResult.meanings.stream().map(meaning -> "<li>" + meaning + "</li>").collect(Collectors.joining("")) + "</ul>" : "";
         return "<p><i>" + searchResult.gender + "</i></p>" + "<p><i>" + searchResult.etymology + "</i></p>" + meaningList;
+    }
+
+    private String stripNewLines(String string) {
+        return string.replaceAll("\n", "").replaceAll("\r", "");
     }
 }
